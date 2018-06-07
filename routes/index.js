0fd9fed08c0116a361bde75a04ec26699a989836
Abhishek Acharya <abhishek@Abhisheks-MacBook-Pro.local>
2018-06-08 02:30:55 +0545

    nothing much

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {  
-  //res.send('Hey! It works!');
+router.get('/', (req, res) => {
+  // const abhi = {
+  //   name: 'Abhi',
+  //   age: 100,
+  //   cool: true
+  // };
+  // res.json(abhi);
+
+  //  res.send('Hey! It works!');
+  // res.json(req.query);
   res.render('hello');
 });
 
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
 module.exports = router;
