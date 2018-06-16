d39b6534298fdba8e6fe9bda1c4111192181a592
Abhishek Acharya <abhishek@Abhisheks-MacBook-Pro.local>
2018-06-17 00:17:41 +0545

    commit2

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,9 @@ router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/stores/page/:page', catchErrors(storeController.getStores));
 router.get('/add', authController.isLoggedIn, storeController.addStore);
-
+router.get('/admin', function (req, res) {
+  res.render('admin');
+});
 router.post('/add',
   storeController.upload,
   catchErrors(storeController.resize),
